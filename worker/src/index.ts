026825211f7896fc17814303f58386c9395486f4
DZWG <paule22@protonmail.com>
2022-09-27 20:32:32 +0200

    added missing local store

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,12 +1,19 @@
 import {run_sync} from "./run_sync";
 import "log-timestamp"
 import {run_loop} from "./run_loop";
-import {DBClient} from "../../libs/library"
+import {DBClient, localStoreInstance} from "../../libs/library"
 
 
 console.log("--- Starting ---");
 
+
 const db = new DBClient()
+const startUp = async () => {
+    await localStoreInstance.init()
+}
+
+startUp().catch((err) => console.log(err))
+
 
 switch (process.env.MODE) {
     case "loop": {
